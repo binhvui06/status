fb489d98f851a0ca20e2c478437b9ccbd4d40d9a
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-03 00:55:23 +0700

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-echo "   1. SETUP"
-echo "   2. DDoS"
+echo "   1. Raven-Storm"
+echo "   2. CC-attack"
 
 echo -e ""
 read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) yum install python3 -y && yum install git -y && cd / && git clone https://github.com/Nghi235/Raven-Storm && cd Raven-Storm && pip3 install -r requirements.txt && python3 main.py
+        1)
+		clear
+		echo "   3. SETUP"
+        echo "   4. DDoS"
+
+        echo -e ""
+        read -p "  Vui Lòng Nhập Một Lựa Chọn 3 Hoặc 4: " abc
+
+    case "${abc}" in
+        3) yum install python3 -y && yum install git -y && cd / && git clone https://github.com/Nghi235/Raven-Storm && cd Raven-Storm && pip3 install -r requirements.txt && python3 main.py
         ;;
-        2) cd /Raven-Storm && python3 main.py
+        4) cd /Raven-Storm && python3 main.py
         ;;
+        *) echo -e "  Chỉ Có Thể Nhập 3 Hoặc 4" && exit
+        ;;
+    esac
+		
+	     ;;
+        2) 
+		
+		clear
+		echo "   5. SETUP"
+        echo "   6. DDoS"
+
+        echo -e ""
+        read -p "  Vui Lòng Nhập Một Lựa Chọn 5 Hoặc 6: " abc
+
+    case "${abc}" in
+        5) yum install python3 -y && yum install git -y && cd / && git clone https://github.com/Nghi235/CC-attack && cd CC-attack && pip3 install requests pysocks && python3 cc.py
+        ;;
+        6) cd /CC-attack && python3 cc.py
+        ;;
+        *) echo -e "  Chỉ Có Thể Nhập 5 Hoặc 6" && exit
+        ;;
+    esac
+		
+       ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && exit
         ;;
     esac
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
