464cb5a3289b86bf0073d5fba2292acf4d98a3c8
binhvui06 <87931707+binhvui06@users.noreply.github.com>
2022-06-06 12:36:58 +0700

    Update nhapdomain.sh

--- nhapdomain.sh
+++ nhapdomain.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "          ----------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo "                                                       ██      ██    ██ ██████"  
+echo "                                                       ██      ██    ██ ██   ██" 
+echo "                                                       ██      ██    ██ ██████"  
+echo "                                                       ██       ██  ██  ██   ██" 
+echo "                                                       ███████   ████   ██████"  
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "          -----------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "                                         Vui Lòng Nhập Domain Của Bạn Để Tiến Hành Quá Trình Cấu Hình Config !"
+echo "                             Vui Lòng Nhập Domain Của Bạn Để Tiến Hành Quá Trình Cấu Hình Config !"
 
 echo -e ""
 echo -e ""
 read -p "Domain: " domain
-if [[ $domain == speed4g.xyz ]]; then
+if [[ $domain == 4gsieutocdo.com ]]; then
 sleep 2
 echo "Đã Xác Thực Domain !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/key.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/key.sh)
 elif [[ $domain == vpndata.xyz ]]; then
 sleep 2
 echo "Đã Xác Thực Domain !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/key.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/key.sh)
 else 
 sleep 2
-echo "Domain Không Chính Xác Hoặc Chưa Được Cho Phép Bởi ADMIN SPEED4G.XYZ !" 
+echo "Domain Không Chính Xác Hoặc Chưa Được Cho Phép Bởi ADMIN 4GSIEUTOCDO.COM !" 
 sleep 1
 rm -rf /etc/XrayR/config.yml
 exit
