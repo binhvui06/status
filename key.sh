9ab2c464ebf6efab27d9e325a589bb2b000703a0
binhvui06 <87931707+binhvui06@users.noreply.github.com>
2022-06-06 12:40:31 +0700

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,38 +1,32 @@
 #!/bin/bash
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "          ----------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo "                                                       ██      ██    ██ ██████"  
+echo "                                                       ██      ██    ██ ██   ██" 
+echo "                                                       ██      ██    ██ ██████"  
+echo "                                                       ██       ██  ██  ██   ██" 
+echo "                                                       ███████   ████   ██████"  
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "          -----------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "                                                                Vui Lòng Nhập Key Của Bạn !"
-echo "                                                      Nếu Không Có Key Vui Lòng Liên Hệ ADMIN SPEED4G.XYZ"
-echo "                                                               ZALO: 0333725953 (Nguyễn Nghị)"
+echo "                                                       Vui Lòng Nhập Key Của Bạn !"
+echo "                                            Nếu Không Có Key Vui Lòng Liên Hệ ADMIN 4GSIEUTOCDO.COM"
+echo "                                                        ZALO: 0357385033 (LVB)"
 echo -e ""
 echo -e ""
 read -p "Key: " key
 
-if [[ $key == nguyenvannghi ]]; then
+if [[ $key == levanbinh ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonspeed4g.sh)
-elif [[ $key == dangvanhoang ]]; then
+bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/luachon4gsieutocdo.sh)
+elif [[ $key == binh ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/luachonvpndata.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/luachon4gsieutocdo.sh)
 else 
 sleep 2
 echo "Key Không Chính Xác !"
