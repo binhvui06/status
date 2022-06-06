cb342c625b105f4a6614f14760f3194bafa142eb
binhvui06 <87931707+binhvui06@users.noreply.github.com>
2022-06-06 12:49:40 +0700

    Update luachon4gsieutocdo.sh

--- luachon4gsieutocdo.sh
+++ luachon4gsieutocdo.sh
@@ -26,7 +26,7 @@ echo -e ""
     case "${num}" in
         1) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/binhvui06/config/main/4gsieutocdo/v2ray/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
-        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/binhvui06/config/main/4gsieutocdo/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/Ngbinhvui06hi235/status/main/setup.sh) && nano /etc/XrayR/config.yml
+        2) cd /etc/XrayR && rm -rf config.yml && curl -OL https://raw.githubusercontent.com/binhvui06/config/main/4gsieutocdo/trojan/config.yml && cd ../.. && bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/setup.sh) && nano /etc/XrayR/config.yml
         ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" && bash <(curl -Ls https://github.com/binhvui06/status/blob/main/luachon4gsieutocdo.sh)
         ;;
